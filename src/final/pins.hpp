c1193d232017aef9c1fc0a1b89f762028a4db364
eclipse <rjc6@rice.edu>
2018-04-23 17:49:48 -0500

    Changed externs to fix issue with adc_info

--- src/final/pins.hpp
+++ src/final/pins.hpp
@@ -6,17 +6,31 @@
 #define SOFTWARE_PINS_HPP
 
 #include <bcm2835.h>
-extern RPiGPIOPin ADC_0_CS;
-extern RPiGPIOPin ADC_1_CS;
-extern RPiGPIOPin ADC_2_CS;
+#define ADC_0_CS RPI_V2_GPIO_P1_26
+#define ADC_1_CS RPI_V2_GPIO_P1_24
+#define ADC_2_CS RPI_V2_GPIO_P1_22
 
-extern RPiGPIOPin MAIN_VALVE;
-extern RPiGPIOPin WATER_VALVE;
-extern RPiGPIOPin VALVE_3;
-extern RPiGPIOPin GITVC_VALVE;
-extern RPiGPIOPin IGN_START;
+#define MAIN_VALVE RPI_V2_GPIO_P1_11
+#define WATER_VALVE RPI_V2_GPIO_P1_13 // Water
+#define VALVE_3 RPI_V2_GPIO_P1_15
+#define GITVC_VALVE RPI_V2_GPIO_P1_18 // GITVC
+#define IGN_START RPI_V2_GPIO_P1_16
 
-extern RPiGPIOPin LC_ADC;
-extern RPiGPIOPin PT_ADC;
-extern RPiGPIOPin TC_ADC;
+#define LC_ADC ADC_2_CS
+#define PT_ADC ADC_1_CS
+#define TC_ADC ADC_1_CS
+
+//extern RPiGPIOPin ADC_0_CS;
+//extern RPiGPIOPin ADC_1_CS;
+//extern RPiGPIOPin ADC_2_CS;
+//
+//extern RPiGPIOPin MAIN_VALVE;
+//extern RPiGPIOPin WATER_VALVE;
+//extern RPiGPIOPin VALVE_3;
+//extern RPiGPIOPin GITVC_VALVE;
+//extern RPiGPIOPin IGN_START;
+//
+//RPiGPIOPin LC_ADC = ADC_2_CS;
+//extern RPiGPIOPin PT_ADC;
+//extern RPiGPIOPin TC_ADC;
 #endif //SOFTWARE_PINS_HPP
